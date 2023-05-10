3df3a558502b5c5f14a7c682088c56495afe2d8a
Gerardo <gerardo@latteandcode.com>
2023-05-10 09:21:00 +0200

    fix: MediaAnalyzer methods don't receive the right arguments

--- src/MediaAnalyzer.php
+++ src/MediaAnalyzer.php
@@ -3,8 +3,7 @@
 namespace LegitHealth\DapiBundle;
 
 use LegitHealth\Dapi\MediaAnalyzer as DapiMediaAnalyzer;
-use LegitHealth\Dapi\MediaAnalyzerArguments\FollowUpArguments;
-use LegitHealth\Dapi\MediaAnalyzerArguments\PredictArguments;
+use LegitHealth\Dapi\MediaAnalyzerArguments\MediaAnalyzerArguments;
 use LegitHealth\Dapi\MediaAnalyzerResponse\MediaAnalyzerResponse;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -17,12 +16,12 @@ class MediaAnalyzer
         $this->dapiMediaAnalyzer = DapiMediaAnalyzer::createWithHttpClient($dapiHttpClient);
     }
 
-    public function predict(PredictArguments $arguments): MediaAnalyzerResponse
+    public function predict(MediaAnalyzerArguments $arguments): MediaAnalyzerResponse
     {
         return $this->dapiMediaAnalyzer->predict($arguments);
     }
 
-    public function followUp(FollowUpArguments $arguments): MediaAnalyzerResponse
+    public function followUp(MediaAnalyzerArguments $arguments): MediaAnalyzerResponse
     {
         return $this->dapiMediaAnalyzer->followUp($arguments);
     }
